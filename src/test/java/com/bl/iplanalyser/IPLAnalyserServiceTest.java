41b7dcc03d46b07a39cd97674ae3dfce049945ee
Jagriti <jagriti.agni@gmail.com>
2020-11-01 00:08:30 +0530

    [Jagriti]ADDED: Test case to check if most runs csv file loaded correctly

--- src/test/java/com/bl/iplanalyser/IPLAnalyserServiceTest.java
+++ src/test/java/com/bl/iplanalyser/IPLAnalyserServiceTest.java
@@ -1,5 +1,22 @@
 package com.bl.iplanalyser;
 
+import com.bl.csvbuilder.CSVException;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class IPLAnalyserServiceTest {
+    private final static String MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private final static String MOST_WKTS_CSV_FILE_PATH = ".src/test/resources/IPL2019FactsheetMostWkts.csv";
+
+    @Test
+    public void givenMostRunCsvFile_whenLoaded_returnCorrectRecords() {
+        try {
+            IPLAnalyserService censusAnalyser = new IPLAnalyserService();
+            int numOfRecords = censusAnalyser.loadMostRunsData(MOST_RUNS_CSV_FILE_PATH);
+            Assert.assertEquals(101, numOfRecords);
+        } catch (CSVException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
