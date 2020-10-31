41c176fefa1e1a321c4a1b6d1388a61fd6fc1bbe
Jagriti <jagriti.agni@gmail.com>
2020-11-01 00:09:16 +0530

    [Jagriti]ADDED: Method and class to load most runs csv file using CSV builder jar

--- src/main/java/com/bl/iplanalyser/IPLAnalyserService.java
+++ src/main/java/com/bl/iplanalyser/IPLAnalyserService.java
@@ -1,7 +1,32 @@
 package com.bl.iplanalyser;
 
+import com.bl.csvbuilder.CSVBuilderFactory;
+import com.bl.csvbuilder.CSVException;
+import com.bl.csvbuilder.ICSVBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class IPLAnalyserService {
-    public static void main(String[] args) {
+    List<MostRunsCSV> mostRunsCSVList = null;
+
+    public void printWelcomeMsg() {
         System.out.println("Welcome to IPL Analyser system.");
+    }
+
+    public int loadMostRunsData(String csvFilePath) throws CSVException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            mostRunsCSVList = csvBuilder.getCSVFileList(reader, MostRunsCSV.class);
+            return mostRunsCSVList.size();
+        } catch (IOException e) {
+            throw new CSVException(e.getMessage(),
+                    CSVException.ExceptionType.CSV_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CSVException(e.getMessage(), CSVException.ExceptionType.WRONG_CSV_FILE);
+        }
     }
 }
---
+++ src/main/java/com/bl/iplanalyser/MostRunsCSV.java
@@ -0,0 +1,60 @@
+package com.bl.iplanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class MostRunsCSV {
+        @CsvBindByName(column = "POS")
+        public int pos;
+
+        @CsvBindByName(column = "PLAYER")
+        public String player;
+
+        @CsvBindByName(column = "Mat")
+        public int mat;
+
+        @CsvBindByName(column = "Inns")
+        public int inns;
+
+        @CsvBindByName(column = "No")
+        public int no;
+
+        @CsvBindByName(column = "Runs")
+        public int runs;
+
+        @CsvBindByName(column = "HS")
+        public String hs;
+
+        @CsvBindByName(column = "Avg")
+        public String avg;
+
+        @CsvBindByName(column = "BF")
+        public int bf;
+
+        @CsvBindByName(column = "SR")
+        public double sr;
+
+        @CsvBindByName(column = "100")
+        public int num100;
+
+        @CsvBindByName(column = "50")
+        public int num50;
+
+        @CsvBindByName(column = "4s")
+        public int num4s;
+
+        @CsvBindByName(column = "6s")
+        public int num6s;
+
+        public double getAverage() {
+            if (avg.equals("-"))
+                return 0.0;
+            return Double.parseDouble(avg);
+        }
+
+        @Override
+        public String toString() {
+            return "MostRunCSV{" + "POS=" + pos + ", Player=" + player + ", Mat=" + mat + ", Inns=" + inns + ", No=" + no
+                    + ", Runs=" + runs + ", HS=" + hs + ", Avg=" + getAverage() + ", BF=" + bf + ", SR=" + sr + ", 100="
+                    + num100 + ", 50=" + num50 + ", 4s=" + num4s + ", 6s=" + num6s + '}';
+        }
+}
